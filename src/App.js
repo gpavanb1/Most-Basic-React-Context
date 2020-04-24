c8f6feef8a9d398075bdceefec26c29e813b45e3
Pavan BG <gpbabc@yahoo.com>
2020-04-24 19:48:05 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    return (
+      <div>
+        <ScoreBoardContainer />
+        <ControllerContainer />
+      </div>
+    );
+}
+
+export default App;
